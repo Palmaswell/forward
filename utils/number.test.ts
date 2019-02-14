315fe8231700466a3cfe01c2fc6f269225254cc9
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-02-14 21:57:40 +0100

    feat(utils): find prime number implementation

--- utils/number.test.ts
+++ utils/number.test.ts
@@ -1,8 +1,13 @@
 import * as Util from './';
 
-test('if a number is prime', () => {
+test('check if a number is a prime number', () => {
   expect(Util.isPrime(11)).toBe(true);
   expect(Util.isPrime(7)).toBe(true);
   expect(Util.isPrime(24)).toBe(false);
   expect(Util.isPrime(10)).toBe(false);
-})
+});
+
+test('find the closest next prime number', () => {
+  expect(Util.findPrime(8)).toBe(11);
+  expect(Util.findPrime(14)).toBe(17);
+});
--- utils/number.ts
+++ utils/number.ts
@@ -4,10 +4,17 @@
  * @param { number } p
  * @param { number } i
 */
-
 export function isPrime(n: number, p = 2, i = 2): boolean {
   if (i <= n - 2) {
     return isPrime(n, p * (i + 1), i + 1 );
   }
   return p % n !== 0;
+}
+
+/**
+ * @name findPrime
+ * @param { number } n
+*/
+export function findPrime(n: number): number {
+  return isPrime(n) ? n : findPrime(n + 1);
 }
