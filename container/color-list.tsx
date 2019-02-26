c018b7b1bf7a7c03f8cd33db58aed729258b221e
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-02-26 14:46:40 +0100

    feat(container): sanatize raw color palette

--- container/color-list.tsx
+++ container/color-list.tsx
@@ -2,51 +2,58 @@ import * as React from 'react';
 import * as Color from '../color';
 import * as Type from '../types';
 
-export function sanitizeColors(colors: Type.Color[]): void | Type.EnhancedColor {
+export function sanitizeColors(colors: Type.Color[]): Type.EnhancedColor[] {
   Color.sort(colors, Color.luminance);
+  const enhancedColors = JSON.parse(JSON.stringify(colors));
   for(let i = colors.length - 1; i >= 0; i--) {
     if (i >= (colors.length - 1) / 2) {
-      (colors[i] as Type.EnhancedColor).aaa = colors.slice(
-        Color.search(
-          colors,
-          colors[i],
-          Type.A11yRatio.aaa,
-          Type.Search.backward
-        )
+      const aaaIndex = Color.search(
+        colors,
+        colors[i],
+        Type.A11yRatio.aaa,
+        Type.Search.backward
       );
-      (colors[i] as Type.EnhancedColor).aa = colors.slice(
-        Color.search(
-          colors,
-          colors[i],
-          Type.A11yRatio.aa,
-          Type.Search.backward
-        )
-      )
+      const aaIndex = Color.search(
+        colors,
+        colors[i],
+        Type.A11yRatio.aa,
+        Type.Search.backward
+      );
+      enhancedColors[i].aaa = aaaIndex === null
+      ? []
+      : JSON.parse(JSON.stringify(colors.slice(0, aaaIndex + 1)));
+      enhancedColors[i].aa = aaIndex === null
+      ? []
+      : JSON.parse(JSON.stringify(colors.slice(0, aaIndex + 1)));
     }
-    else if (i <= (colors.length - 1) / 2) {
-      (colors[i] as Type.EnhancedColor).aaa = colors.slice(
-        Color.search(
-          colors,
-          colors[i],
-          Type.A11yRatio.aaa
-        )
+    else {
+      const aaaIndex = Color.search(
+        colors,
+        colors[i],
+        Type.A11yRatio.aaa
       );
-      (colors[i] as Type.EnhancedColor).aa = colors.slice(
-        Color.search(
-          colors,
-          colors[i],
-          Type.A11yRatio.aa
-        )
-      )
+      const aaIndex = Color.search(
+        colors,
+        colors[i],
+        Type.A11yRatio.aa,
+      );
+      enhancedColors[i].aaa = aaaIndex === null
+      ? []
+      : JSON.parse(JSON.stringify(colors.slice(-aaaIndex)));
+      enhancedColors[i].aa = aaIndex === null
+      ? []
+      : JSON.parse(JSON.stringify(colors.slice(-aaIndex)));
     }
   }
+
+  return enhancedColors;
 }
 
 
 export function ColorList(): JSX.Element {
   const [count, setCount] = React.useState(0);
-  sanitizeColors(Color.palette);
-
+  const foo = sanitizeColors(Color.palette);
+  console.log(foo[0], '*****');
   return (
     <>
     <h1>Hello everyone 👋</h1>
