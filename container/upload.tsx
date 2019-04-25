9e744f007f06cc384c5ebf037dc043ef81251450
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-04-25 22:33:42 +0200

    WIP feat(container): unit tests for the upload container

--- container/upload.tsx
+++ container/upload.tsx
@@ -1,36 +1,16 @@
 import * as React from 'react';
 import uuid from 'uuid/v3';
-import Cookie from 'js-cookie';
+// import Cookie from 'js-cookie';
 
-export class Upload extends React.Component {
-  public state = {
-    file: null
-  }
-
-  private handleSelectedFile(e: React.ChangeEvent<HTMLInputElement>): void {
-    e.persist();
-    this.setState({...this.state, file: e.target.files[0]});
-    const reader = new FileReader();
-    reader.onload = e => {
-      console.log(reader.result);
-    };
-    reader.readAsText(e.target.files[0]);
-  }
-
-  public componentDidUpdate(): void {
-    Cookie.set('file', this.state.file);
-  }
-
-  public render(): JSX.Element {
-    return (
-      <>
+export const Upload: React.SFC = (): JSX.Element => {
+  return (
+    <>
       <input
+        accept=".json"
         id={uuid('upload', '1b671a64-40d5-491e-99b0-da01ff1f3341')}
         type="file"
-        onChange={e => this.handleSelectedFile(e)}
         multiple
         />
-      </>
-    )
-  }
+    </>
+  )
 }
