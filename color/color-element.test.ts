2d41b0414332b42d9f2ae454c45860ed86e13fad
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-05-20 22:09:52 +0200

    feat(color): color element implementation

--- color/color-element.test.ts
+++ color/color-element.test.ts
@@ -1,11 +1,31 @@
 import * as Color from './';
+import * as Type from '../types';
+const colors: Type.Color[] = [
+  {
+    name: 'Black',
+    rgb: [0, 0, 0]
+  },
+  {
+    name: 'Electromagnetic',
+    rgb: [47, 54, 64]
+  },
+  {
+    name: 'Dracula Orchid',
+    rgb: [45, 52, 54]
+  },
+  {
+    name: 'Blue Nights',
+    rgb: [53, 59, 72]
+  },
+];
 describe('Color Element', () => {
   test('Creates a new ColorElement instance', () => {
-    expect(Color.createElement([245, 246, 250], 'Lynx White')).toMatchObject({
-      rgb: [245, 246, 250],
-      name: 'Lynx White',
-      aa: [],
-      aaa: []
-    })
-  })
+    const colorEl = Color.createElement([245, 246, 250], 'Lynx White');
+    expect(colorEl.getName()).toBe('Lynx White');
+    expect(colorEl.getRGB()).toEqual([245, 246, 250]);
+    expect(colorEl.getRGBString()).toBe('rgb(245, 246, 250)');
+    expect(colorEl.getRGBAString(.5)).toBe('rgba(245, 246, 250, 0.5)');
+    expect(colorEl.getHEXString()).toBe('#f5f6fa');
+    expect(colorEl.getHSLString()).toBe('hsl(228, 33%, 97%)');
+  });
 });
--- color/color-element.ts
+++ color/color-element.ts
@@ -1,18 +1,57 @@
+import tinyColor from 'tinycolor2';
+import * as Color from './';
 import * as Type from '../types';
+
 export interface ColorElementContext {
-  rgb: Type.RGB;
-  name?: string;
-  aa?: ColorElementContext[];
-  aaa?: ColorElementContext[];
+  getName(): string;
+  getAA(): Type.Color[];
+  getAAA(): Type.Color[];
+  getRGB(): Type.RGB;
+  getRGBString(): string;
+  getHEXString(): string;
+  getHSLString(): string;
+  getRGBAString(alpha: number): string;
+  setAA(colors: Type.Color[]): void;
+  setAAA(colors: Type.Color[]): void;
 }
 
-export function createElement(rgb: Type.RGB, name: string) {
-  const aa = [];
-  const aaa = [];
+export function createElement(rgb: Type.RGB, name: string): ColorElementContext {
+  const internalRGB = rgb;
+  const internalName = name;
+  let aa: Type.Color[];
+  let aaa: Type.Color[];
   return {
-    rgb,
-    name,
-    aa,
-    aaa
+    getName() {
+      return internalName;
+    },
+    getRGB() {
+      return internalRGB;
+    },
+    getAA() {
+      return aa;
+    },
+    getAAA() {
+      return aaa;
+    },
+    getRGBString() {
+      return Color.toRGBString(internalRGB);
+    },
+    getHEXString() {
+      return tinyColor(Color.toRGBString(internalRGB)).toHexString();
+    },
+    getHSLString() {
+      return tinyColor(Color.toRGBString(internalRGB)).toHslString();
+    },
+    getRGBAString(alpha: number) {
+      const color = tinyColor(Color.toRGBString(internalRGB));
+      color.setAlpha(alpha);
+      return color.toRgbString();
+    },
+    setAA(colors: Type.Color[]) {
+      aa = colors;
+    },
+    setAAA(colors: Type.Color[]) {
+      aaa = colors;
+    }
   }
 }
